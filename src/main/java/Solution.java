b6e38e7d63dc5b69be5b690a88680e6e4d5e79aa
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-24 20:38:52 -0400

    cant get random tests to pass

---
+++ src/main/java/Solution.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+
+public class Solution {
+
+    public static String dashatize(int num) {
+        int absolute = Math.abs(num);
+        if (num == Integer.MIN_VALUE){
+            absolute = Integer.MAX_VALUE;
+        }
+        String stringNum = String.valueOf(absolute);
+        String[] splitNum = stringNum.split("");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < splitNum.length; i++){
+            boolean isOdd = splitNum[i].equals("1") || splitNum[i].equals("3") || splitNum[i].equals("5") || splitNum[i].equals("7") || splitNum[i].equals("9");
+            if (i != 0 && isOdd){
+                if (i < splitNum.length - 1 && splitNum[i].equals(splitNum[i - 1]) && !splitNum[i].equals(splitNum[i + 1])){
+                    sb.append("-");
+                    sb.append(splitNum[i]);
+                    sb.append("-");
+                    continue;
+                }
+                if (splitNum[i].equals(splitNum[i - 1])){
+                    sb.append("-");
+                    sb.append(splitNum[i]);
+                    continue;
+                }
+
+                if (num == Integer.MIN_VALUE && i == splitNum.length - 1) {
+                    splitNum[splitNum.length - 1] = "8";
+                    sb.append(splitNum[i]);
+                    break;
+                }
+                if (i == splitNum.length - 1){
+                    sb.append("-");
+                    sb.append(splitNum[i]);
+                    break;
+                }
+                if (splitNum[i].equals(splitNum[i + 1])){
+                sb.append(splitNum[i]);
+                continue;
+                }
+                sb.append("-");
+                sb.append(splitNum[i]);
+                sb.append("-");
+            } else {
+                sb.append(splitNum[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+}
