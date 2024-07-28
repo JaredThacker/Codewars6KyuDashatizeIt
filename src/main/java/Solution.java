1d12619f74ca8582f2d2d112830fb7e2919359d1
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-28 19:43:53 -0400

    solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,55 +1,65 @@
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class Solution {
 
     public static String dashatize(int num) {
-        int absolute = Math.abs(num);
-        //should probably remove this
-        if (num == Integer.MIN_VALUE){
-            absolute = Integer.MAX_VALUE;
-        }
-        String stringNum = String.valueOf(absolute);
-        String[] splitNum = stringNum.split("");
-        StringBuilder sb = new StringBuilder();
 
-        //def refactor this somehow! since random tests arent passing i mustve hardcoded somewhere
-        for (int i = 0; i < splitNum.length; i++){
-            boolean isOdd = splitNum[i].equals("1") || splitNum[i].equals("3") || splitNum[i].equals("5") || splitNum[i].equals("7") || splitNum[i].equals("9");
-            if (i != 0 && isOdd){
-                if (i < splitNum.length - 1 && splitNum[i].equals(splitNum[i - 1]) && !splitNum[i].equals(splitNum[i + 1])){
-                    sb.append("-");
-                    sb.append(splitNum[i]);
-                    sb.append("-");
-                    continue;
-                }
-                if (splitNum[i].equals(splitNum[i - 1])){
-                    sb.append("-");
-                    sb.append(splitNum[i]);
-                    continue;
-                }
+         return Integer.toString(num).replaceAll("([13579])","-$1-")
+                 .replaceAll("--","-")
+                 .replaceAll("^-","")
+                 .replaceAll("-$","");
 
-                if (num == Integer.MIN_VALUE && i == splitNum.length - 1) {
-                    splitNum[splitNum.length - 1] = "8";
-                    sb.append(splitNum[i]);
-                    break;
-                }
-                if (i == splitNum.length - 1){
-                    sb.append("-");
-                    sb.append(splitNum[i]);
-                    break;
-                }
-                if (splitNum[i].equals(splitNum[i + 1])){
-                sb.append(splitNum[i]);
-                continue;
-                }
-                sb.append("-");
-                sb.append(splitNum[i]);
-                sb.append("-");
-            } else {
-                sb.append(splitNum[i]);
-            }
-        }
-        return sb.toString();
+//        String strNum = String.valueOf(num).replaceAll("-", "");
+//        ArrayList<String> elems = new ArrayList<>();
+//
+//        HashSet<String> odds = new HashSet<>(List.of("1", "3", "5", "7", "9"));
+//
+//        for (String eachNum : strNum.split("")) {
+//            if (odds.contains(eachNum)) {
+//                elems.add(String.format("-%s-", eachNum));
+//            } else {
+//                elems.add(eachNum);
+//            }
+//        }
+//
+//        return String.join("", elems).replaceAll("--", "-")
+//                .replaceAll("^-", "")
+//                .replaceAll("-$", "");
+
     }
 
+//    public static String dashatize(int num) {
+//
+//        String stringNum = String.valueOf(num).replaceAll("-", "");
+//        String[] splitNum = stringNum.split("");
+//        StringBuilder sb = new StringBuilder();
+//
+//        //def refactor this somehow! since random tests arent passing i mustve hardcoded somewhere
+//        for (int i = 0; i < splitNum.length; i++){
+//            boolean isOdd = "13579".contains(splitNum[i]);
+//            if (isOdd){
+//                if (i == splitNum.length - 1 && stringNum.length() > 1){
+//                    sb.append("-");
+//                    sb.append(splitNum[i]);
+//                    break;
+//                }
+//
+//                if (i != 0) {
+//                    sb.append("-");
+//                }
+//
+//                sb.append(splitNum[i]);
+//
+//                if (stringNum.length() > 1) {
+//                    sb.append("-");
+//                }
+//            } else {
+//                sb.append(splitNum[i]);
+//            }
+//        }
+//
+//        return sb.toString().replaceAll("--", "-");
+//    }
 }
